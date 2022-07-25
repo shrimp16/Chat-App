59cdd8d68d117e4d924b407ad2298671ef040a28
Shrimp <lcamara0916@gmail.com>
2022-07-25 16:39:18 +0000

    Added a temporary way to identify users by giving them a username on the client side

--- client/script.js
+++ client/script.js
@@ -2,11 +2,13 @@ const socket = new WebSocket('ws://localhost:13000')
 
 let message = document.getElementById('message');
 
+let un = prompt('username');
+
 socket.onmessage = ({ data }) => {
     console.log(data);
 }
 
 document.getElementById('send').addEventListener('click', () => {
-    socket.send(message.value);
+    socket.send(`${un} : ${message.value}`);
     message.value = '';
 })
