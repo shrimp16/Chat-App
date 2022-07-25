993d1fc87413700ae4a1c915fc72ad86dfbc91d0
Shrimp <lcamara0916@gmail.com>
2022-07-25 16:46:03 +0000

    Every client socket now recieves a unique id on the server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,20 @@ const WebSocket = require('ws');
 
 const WebSocketServer = new WebSocket.Server({ port: '13000' });
 
+function getUniqueID() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+};
+
 WebSocketServer.on('connection', (WebSocket) => {
 
+    WebSocket.id = getUniqueID();
+
     WebSocket.on('message', (data, isBinary) => {
+
+        console.log(WebSocket.id);
 
         WebSocketServer.clients.forEach((client) => {
 
