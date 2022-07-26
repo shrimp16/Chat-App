b9d4d79100c9b395dfc4bd69512e6ab8a0430d1f
Shrimp <lcamara0916@gmail.com>
2022-07-26 13:31:33 +0000

    Deleted unnecessary code

--- server/server.js
+++ server/server.js
@@ -7,18 +7,6 @@ let rooms = {
     admin: []
 }
 
-function getRoomIndex(roomId) {
-
-    for (let i = 0; i < rooms.length; i++) {
-        if (rooms[i].id === roomId) {
-            return i;
-        }
-    }
-
-    return 'not found';
-
-}
-
 function getUniqueID() {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
@@ -28,9 +16,6 @@ function getUniqueID() {
 
 function changeRoom(room, newRoom, user) {
 
-    console.log(room);
-    console.log(newRoom);
-
     let usersCopy = rooms[room];
 
     usersCopy = usersCopy.filter(element => element.id !== user.id);
@@ -39,31 +24,14 @@ function changeRoom(room, newRoom, user) {
 
     rooms[newRoom].push(user);
 
-    console.log(rooms);
-
-}
-
-function isOnRoom(room, user) {
-
-    let userList = rooms[getRoomIndex(room)].users;
-
-    for (let i = 0; i < userList.length; i++) {
-        if (userList[i] === user.id) {
-            user.send('ayo lmao');
-        }
-    }
-
 }
 
 WebSocketServer.on('connection', (WebSocket) => {
 
     WebSocket.id = getUniqueID();
 
-    
     rooms.room.push(WebSocket);
     
-    console.log(rooms);
-
     WebSocket.on('message', (data, isBinary) => {
 
         let JSONData = JSON.parse(data);
@@ -81,15 +49,6 @@ WebSocketServer.on('connection', (WebSocket) => {
                 break;
 
         }
-
-        /*WebSocketServer.clients.forEach((client) => {
-
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data, { binary: isBinary });
-            }
-
-        })*/
-
     })
 
 })
