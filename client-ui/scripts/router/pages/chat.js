b1f8b82838bbfc8c5b4017da64f1b0f62c19594d
Shrimp <lcamara0916@gmail.com>
2022-07-30 13:38:12 +0000

    Delete example message

--- client-ui/scripts/router/pages/chat.js
+++ client-ui/scripts/router/pages/chat.js
@@ -16,9 +16,6 @@ export default class Chat {
 
         this.body.innerHTML = `
             <div class="message-container" id="message-container">
-                <div class="message">
-                    <p class="message-text">This is a message</p>
-                </div>
             </div>
             <div class="message-input">
                 <input id="message-input" placeholder="Message..." type="text">
